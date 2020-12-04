b25030b359c2b87362225fca2af18cc9057c910c
Fireal1983 <Fireal1983@github.com>
2020-12-04 13:45:28 -0500

    Links working 90%

---
+++ Assets/Scripts/NodeLink/LinkView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkView : MonoBehaviour
+{
+    // corresponding node
+    public Link _link;
+
+    // object / geometry
+    public void Init(Link _link)
+    {
+        gameObject.name = "Link" + _link.linkIndex.ToString();
+        gameObject.transform.position = _link.startPos;
+        //gameObject.GetComponent<LineRenderer>();
+    }
+    
+}
