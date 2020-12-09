0f990a3b6bd1826b13e252d6939366d192b88ddc
johnathan <xxsedohrxx@hotmail.com>
2020-12-09 15:35:23 +0100

    Updated the StoryElementClass

--- Assets/StoryApp/Scripts/Story/StoryElement.cs
+++ Assets/StoryApp/Scripts/Story/StoryElement.cs
@@ -2,25 +2,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class StoryElement
 {
+    // story, id, title, image, description, position, parent, children, haschoice, choiceID
     #region Properties
     public int _id { get; }
     public int _idOfStory { get; }
     public string _title { get; }
     public string _description { get; }
     public bool _hasChoice { get; }
-    #endregion
-    #region StoryElementConstructor
-    
-    public StoryElement(int id, string title, string description, bool hasChoice, int idOfStory)
+    public int _choiceID { get; }
+    public Vector3 _position { get; }
+    Node _parent { get; }
+    List<Node> _children { get; }
+    Image _image { get; }
+
+#endregion
+#region StoryElementConstructor
+
+public StoryElement(int id, int idOfStory, string title, string description, bool hasChoice, 
+    int choiceID, Vector3 position, Node parent, List<Node> children, Image image)
     {
         this._id = id;
+        this._idOfStory = idOfStory;
         this._title = title;
         this._description = description;
         this._hasChoice = hasChoice;
-        this._idOfStory = idOfStory;
+        this._choiceID = choiceID;
+        this._position = position;
+        this._parent = parent;
+        this._children = children;
+        this._image = image;
     }
     #endregion
 
