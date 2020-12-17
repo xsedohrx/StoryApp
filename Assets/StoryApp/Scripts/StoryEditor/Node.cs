fca60ddd7cbecb6c1e55a5ff62e44be62607682a
Fireal1983 <Fireal1983@github.com>
2020-12-16 23:58:44 -0500

    12-16-2020 - Solid Save Point
    Linkages working correctly, Todo create Node delete function and refactor

--- Assets/StoryApp/Scripts/StoryEditor/Node.cs
+++ Assets/StoryApp/Scripts/StoryEditor/Node.cs
@@ -16,16 +16,36 @@ public class Node : MonoBehaviour
         closed,
     }
 
-    //Fields
-    public Color nodeHighlightColor;
-    public Color nodeNoHighlightColor;
+    public enum NodeColor
+    {
+        draggable,
+        highlighted,
+        selectable
+    }
+
+    public enum ProcessOutLinks
+    {
+        linkA = 0,
+        linkB = 1,
+        linkC = 2,
+        linkD = 3
+    }
     // ----------------------------------
 
     // Properties
     public Access access { get; set; }
-    public string nodeName { get; set; }
+    public NodeColor nodeColor { get; set; }
+    public ProcessOutLinks processOutLinks { get; set; }
     public int nodeID { get; set; }
     // ----------------------------------
+    //Serialized Fields
+    public Color Red = Color.red;
+    public Color Green = Color.green;
+    public Color Blue = Color.blue;
+    public Color Purple = Color.magenta;
+    // ----------------------------------
+
+
 
     //INPUT NODES
     public GameObject inNode { get; set; }
@@ -34,8 +54,10 @@ public class Node : MonoBehaviour
     public SortedDictionary<string, GameObject> inNodeDictionary { get; set; }
 
     // OUTPUTS NODES
+    [SerializeField] public int outNodeCount;
     public GameObject outNodeA { get; set; }
     public GameObject outNodeB { get; set; }
+    public SortedDictionary<string, GameObject> outNodeDictionary { get; set; }
     // ----------------------------------
 
     // INPUTS LINKS
@@ -49,44 +71,80 @@ public class Node : MonoBehaviour
     public int maxOutLinks { get; set; }
     public GameObject outLinkA { get; set; }
     public GameObject outLinkB { get; set; }
+    public GameObject outLinkC { get; set; }
+    public GameObject outLinkD { get; set; }
     public SortedDictionary<string, GameObject> outLinkDictionary { get; set; }
     // ----------------------------------
 
 
-
-
-    public static bool queriesHitTriggers = true;   // ensures this object remains a HitTrigger for collider
-
-
     private void Awake()
     {
-        nodeName = "node";
         inNodeCount = 0;
         inNodeDictionary = new SortedDictionary<string, GameObject>();
+
+        outNodeCount = 0;
+        outNodeDictionary = new SortedDictionary<string, GameObject>();
 
         inLinkCount = 0;
         inLinkDictionary = new SortedDictionary<string, GameObject>();
 
-        maxOutLinks = 2;
+        maxOutLinks = 4;
         outLinkCount = 0;
         outLinkDictionary = new SortedDictionary<string, GameObject>();
 
-        this.gameObject.GetComponent<MeshRenderer>().material.color = this.nodeNoHighlightColor;
-        this.access = Access.open;
+        access = Access.open;
+        ColorSelect(NodeColor.draggable);
     }
-
-    public bool isCapacity()
-    {
-        return this.outLinkCount == this.maxOutLinks ? true : false;
-    }
-
-    public string ReturnName()
-    {
-        return this.nodeName;
-    }
-
 
     // Node Helper Functions
+    public void ColorSelect(NodeColor nodeColor)
+    {
+        switch (nodeColor)
+        {
+            case NodeColor.draggable:
+                gameObject.GetComponent<MeshRenderer>().material.color = new Color(0.3f, 0.3f, 0.3f);
+                break;
+            case NodeColor.highlighted:
+                gameObject.GetComponent<MeshRenderer>().material.color = new Color(0.9f, 1, 0);
+                break;
+            case NodeColor.selectable:
+                gameObject.GetComponent<MeshRenderer>().material.color = new Color(0.9f, 1, 0);
+                break;
+        }
+    }
+    public void AddInNodeDict(string nodeName, GameObject gameObject)
+    {
+        if (!inNodeDictionary.ContainsKey(nodeName))
+        {
+            inNodeDictionary.Add(nodeName, gameObject);
+            inNodeCount = inNodeDictionary.Count;
+        }
+        else
+        {
+            Debug.Log("These Nodes are already connected");
+        }
+    }
+    public void RemoveInNodeDict(string nodeName)
+    {
+        if (inNodeDictionary.TryGetValue(nodeName, out GameObject gameObject))
+        {
+            inNodeDictionary.Remove(nodeName);
+            //Destroy(linkObject);
+            inNodeCount = inNodeDictionary.Count;
+        }
+    }
+
+    public void AddOutNodeDict(string nodeName, GameObject gameObject)
+    {
+        if (!outNodeDictionary.ContainsKey(nodeName))
+        {
+            outNodeDictionary.Add(nodeName, gameObject);
+        }
+        else
+        {
+            Debug.Log("These Nodes are already connected");
+        }
+    }
     public bool duplicateNodeConnectionCheck(string nodeName)
     {
         if (inNodeDictionary.ContainsKey(nodeName))
@@ -100,28 +158,46 @@ public class Node : MonoBehaviour
             return false;
         }
     }
-    public void AddInNodeDict(string nodeName, GameObject gameObject)
+    public bool isCapacity()
     {
-        if (!inNodeDictionary.ContainsKey(nodeName))
-        {
-            inNodeDictionary.Add(nodeName, gameObject);
-        }
-        else
-        {
-            Debug.Log("These Nodes are already connected");
-        }
+        return this.outLinkCount == this.maxOutLinks ? true : false;
     }
-    public void RemoveInNodeDict(string linkName)
+    // ---------------------
+
+    // Link Helper Functions
+
+    public void AssignOutLink(int linknumber, GameObject outLink, GameObject outNodeObj, out string name, out Color lineColor)
     {
-        if (inNodeDictionary.TryGetValue(linkName, out GameObject linkObject))
+        processOutLinks = (ProcessOutLinks)linknumber;
+        switch (processOutLinks)
         {
-            inNodeDictionary.Remove(linkName);
-            Destroy(linkObject);
-            inNodeCount = inNodeDictionary.Count;
+            case ProcessOutLinks.linkA:
+                name = outNodeObj.name + "Link" + "A";
+                this.outLinkA = outLink;
+                lineColor = Red;
+                break;
+            case ProcessOutLinks.linkB:
+                name = outNodeObj.name + "Link" + "B";
+                this.outLinkB = outLink;
+                lineColor = Green;
+                break;
+            case ProcessOutLinks.linkC:
+                name = outNodeObj.name + "Link" + "C";
+                this.outLinkC = outLink;
+                lineColor = Blue;
+                break;
+            case ProcessOutLinks.linkD:
+                name = outNodeObj.name + "Link" + "D";
+                this.outLinkD = outLink;
+                lineColor = Purple;
+                break;
+            default:
+                name = "No Link?";
+                lineColor = new Color(1f, 0f, 0f);
+                break;
         }
     }
 
-    // Link Helper Functions
     public void AddOutLinkDict(string nodeName, GameObject gameObject)
     {
         if (!outLinkDictionary.ContainsKey(nodeName))
@@ -152,19 +228,11 @@ public class Node : MonoBehaviour
         if (inLinkDictionary.TryGetValue(linkName, out GameObject linkObject))
         {
             inLinkDictionary.Remove(linkName);
-            Destroy(linkObject);
+            //Destroy(linkObject);
             inLinkCount = inLinkDictionary.Count;
         }
     }
-
-    public void DragOutNodes(Vector3 mousePos)
-    {
-        foreach (KeyValuePair<string, GameObject> valuePair in outLinkDictionary)
-        {
-            valuePair.Value.GetComponent<LineRenderer>().SetPosition(0, mousePos);
-        }
-    }
-    public void DragInNodes(Vector3 mousePos)
+    public void DragInLinks(Vector3 mousePos)
     {
         foreach (KeyValuePair<string, GameObject> valuePair in inLinkDictionary)
         {
@@ -178,10 +246,17 @@ public class Node : MonoBehaviour
             }
         }
     }
-
-    public GameObject LinkSpawner(GameObject nodeGameObj, out LineRenderer lineLink, out Link currentLink, out string linkName, out GameObject currentGO)
+    public void DragOutLinks(Vector3 mousePos)
     {
-        currentGO = Instantiate(linkPrefab, nodeGameObj.transform.position, Quaternion.identity, nodeGameObj.transform);
+        foreach (KeyValuePair<string, GameObject> valuePair in outLinkDictionary)
+        {
+            valuePair.Value.GetComponent<LineRenderer>().SetPosition(0, mousePos);
+        }
+    }
+    // Spawn Link start position
+    public GameObject LinkSpawner(GameObject nodeGameObj, out LineRenderer lineLink, out CapsuleCollider capsule, out Link currentLink, out string linkName, out GameObject currentGO)
+    {
+        currentGO = Instantiate(linkPrefab, nodeGameObj.transform, true);
         {
             //Assignments
             currentLink = currentGO.GetComponent<Link>();
@@ -189,151 +264,72 @@ public class Node : MonoBehaviour
             lineLink = currentLink.GetComponent<LineRenderer>();
 
             // Link Assignments
-            currentLink.name = startNode.outLinkCount == 0 ? startNode.name + "Link" + "A" : startNode.name + "Link" + " B";
-            linkName = currentLink.name;
+            //currentLink.name = startNode.outLinkCount == 0 ? startNode.name + "Link" + "A" : startNode.name + "Link" + " B";
+
             currentLink.linkIndex = 0;
             currentLink.parentObject = nodeGameObj.transform;
             currentLink.startPos = nodeGameObj.transform.position;
-
             currentLink.linkOriginNode = nodeGameObj;
 
             //Node Assignments for the node that has just been clicked on to initiate the LineRenderer;
+            AssignOutLink(outLinkCount, currentGO, nodeGameObj, out string name, out Color lineColor);
 
-
-            if (startNode.outLinkCount == 0)
-            {
-                startNode.outLinkA = currentGO;
-                lineLink.material = currentLink.Red;
-            }
-            else
-            {
-                startNode.outLinkB = currentGO;
-                lineLink.material = currentLink.Blue;
-            }
+            linkName = name;
+            currentLink.name = name;
 
             // LineRenderer Start Position Assignments
-            //startNode.state = Node.State.selectable;
             lineLink.enabled = true;
+            lineLink.material = new Material(Shader.Find("Sprites/Default"));
+            lineLink.startColor = lineColor;
+            lineLink.endColor = lineColor;
             lineLink.positionCount = 2;
             lineLink.startWidth = 0.2f;
-            lineLink.SetPosition(0, currentLink.startPos);
+            lineLink.SetPosition(0, nodeGameObj.transform.position);
+            lineLink.SetPosition(1, nodeGameObj.transform.position);
 
-            // Add Link GameObejct to List
+
+            // Edge Collider Assignments
+            capsule = currentGO.GetComponent<CapsuleCollider>();
+            capsule.radius = lineLink.startWidth * 0.5f;
+            capsule.center = Vector3.zero;
+            capsule.direction = 2; // Z-axis for easier "LookAt" orientation
         }
-        //dataContainer.AddGameObjList(currentGO);
-        AddOutLinkDict(linkName, currentGO);
+        // Add Link GameObejct to List
         return currentGO;
     }
+    // ---------------------
+    // Collider Helper Functions
+    public void DragInColliders(Vector3 endPos)
+    {
+        foreach (KeyValuePair<string, GameObject> valuePair in inLinkDictionary)
+        {
+            for (int i = 0; i < valuePair.Value.transform.childCount; i++)
+            {
+                Debug.Log(valuePair.Key);
+                if (valuePair.Value.transform.GetChild(i).gameObject.name == valuePair.Key.ToString())
+                {
+                    Vector3 startPos = valuePair.Value.transform.position;
+                    //Vector3 endPos = this.gameObject.transform.position;
 
+                    CapsuleCollider capsule = valuePair.Value.transform.GetChild(i).gameObject.GetComponent<CapsuleCollider>();
+                    capsule.transform.position = startPos + (endPos - startPos) / 2;
+                    capsule.transform.LookAt(startPos);
+                    capsule.height = (endPos - startPos).magnitude;
+                }
+            }
+        }
+    }
+    public void DragOutColliders(Vector3 startPos)
+    {
+        foreach (KeyValuePair<string, GameObject> valuePair in outLinkDictionary)
+        {
+            Vector3 endPos = valuePair.Value.GetComponent<Link>().linkDestinationNode.gameObject.transform.position;
 
-    //public void HighlightToggle(State state)
-    //{
-    //    switch (state)
-    //    {
-    //        case State.draggable:
-    //            this.gameObject.GetComponent<MeshRenderer>().material.color = nodeNoHighlightColor;
-    //            break;
-    //        case State.highlighted:
-    //            this.gameObject.GetComponent<MeshRenderer>().material.color = new Color(0.9f, 1, 0);
-    //            break;
-    //        case State.selectable:
-    //            this.gameObject.GetComponent<MeshRenderer>().material.color = new Color(0.9f, 1, 0);
-    //            break;
-    //        case State.linking:
-    //            this.gameObject.GetComponent<MeshRenderer>().material.color = new Color(0.9f, 1, 0);
-    //            break;
-    //    }
-    //}
+            CapsuleCollider capsule = valuePair.Value.GetComponent<CapsuleCollider>();
+            capsule.transform.position = startPos + (endPos - startPos) / 2;
+            capsule.transform.LookAt(startPos);
+            capsule.height = (endPos - startPos).magnitude;
+        }
+    }
+    // ---------------------
 }
-
-
-//    Func<int> getRandomNumber = () => new Random().Next(1, 100);
-
-//    //Or 
-
-//    Func<int, int, int> Sum = (x, y) => x + y;
-
-
-
-
-
-
-
-    // OLD CODE - DON'T DELETE
-    //// OnMouseDown
-    //private void OnMouseDown()
-    //{
-    //    ReturnDownNode();
-    //}
-    //public void ReturnDownNode()
-    //{
-    //    Controller.Instance.returnHitDownNodeGO = this.gameObject;
-    //}
-
-    //// OnMouseUp
-    //private void OnMouseOver()
-    //{
-    //    ReturnOverNode();
-    //}
-    //public void ReturnOverNode()
-    //{
-    //    Controller.Instance.returnHitUpNodeGO = this.gameObject;
-    //}
-
-    //void OnMouseExit()
-    //{
-    //    ReturnOffNode();
-    //}
-    //public void ReturnOffNode()
-    //{
-    //    Controller.Instance.returnHitUpNodeGO = null;
-    //}
-
-    //// OnMouseButtonUp (Up on the same object)
-    //private void OnMouseUpAsButton()
-    //{
-    //    ReturnUpButtonNode();
-    //}
-    //public void ReturnUpButtonNode()
-    //{
-    //    Controller.Instance.returnHitButtonUpNodeGO = this.gameObject;
-    //}
-
-    //public GameObject ReturnDownGameobject()
-    //{
-    //    Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
-    //    RaycastHit hitInfo;
-
-    //    if (Physics.Raycast(rayOrigin, out hitInfo))
-    //    {
-    //        var obj = hitInfo.collider.gameObject;
-    //        if (obj != null)
-    //        {
-    //            //Debug.Log("This was called from the Node" + obj.ToString());
-    //            return obj;
-    //        }
-    //    }
-    //    Debug.Log("Object Inactive - Might receive a null error");
-    //    return null;
-    //}
-
-
-
-
-    //public GameObject ReturnUpGameobject()
-    //{
-    //    Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
-    //    RaycastHit hitInfo;
-
-    //    if (Physics.Raycast(rayOrigin, out hitInfo))
-    //    {
-    //        var obj = hitInfo.collider.gameObject; // This!
-    //        if (obj != null)
-    //        {
-    //            //Debug.Log(obj.ToString());
-    //            return obj;
-    //        }
-    //    }
-    //    Debug.Log("Object Inactive - Might receive a null error");
-    //    return null;
-    //}
